1652e53d23baf602b901e3705f65235779dd87a3
Sanghee Kim <sangheestyle@gmail.com>
2019-06-16 12:11:35 +0300

    support GET /articles

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const articles = [
+  {
+    title: "article 1 title",
+    body: "article 1 body"
+  },
+  {
+    title: "article 2 title",
+    body: "article 2 body"
+  }
+];
+
+app.get("/articles", (req, res) => res.send(articles));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
