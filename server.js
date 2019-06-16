30cd7b9f167e8b0fc6ad9e099e5035e7e5702426
Sanghee Kim <sangheestyle@gmail.com>
2019-06-16 12:44:17 +0300

    we can see all the requests

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const morgan = require("morgan");
 const DB = require("./db");
 
 const db = new DB();
 
 const app = express();
 const port = 3000;
+
+app.use(morgan("tiny"));
 
 app.get("/articles", (req, res) => res.send(db.getArticles()));
 app.get("/articles/title", (req, res) => {
